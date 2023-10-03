c2f60a193cc252b14fc6245660f6122af82f060a
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2023-10-03 16:51:16 -0700

    Working on making the app Resizable

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'
 import App from './App';
-
-import { ToastContainer } from 'react-toastify';
 
 import { AppProvider } from './redux/store';
 
