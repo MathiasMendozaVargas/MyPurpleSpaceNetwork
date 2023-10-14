43096ce360e80824e14c6a0c8aff7039af5a4958
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2023-10-13 22:37:29 -0700

    Giving final touches to 3D model of Login Page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,19 +4,19 @@ import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabaseClient';
 import { setUser } from '../redux/slice/userSlice';
-import { ToastContainer, toast, cssTransition } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
+// 3D Earth Model
+import { EarthCanvas } from '../components/Canvas/Earth'
 
 // importing Navbar
 import Navbar from '../components/Navbar'
 
 
-// importing media files
-
-
 // Login Page Template
-
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     const emailRef = useRef()
@@ -25,7 +25,9 @@ const Login = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    
+    // Framer Motion
+    const control = useAnimation()
+    const [formRef, inView] = useInView()
 
     async function getUserMetaData(current_user_id) {
         const { data, error } = await supabase.from('users_data').select()
@@ -81,47 +83,48 @@ const Login = () => {
 
     return (
         <>
+            <ToastContainer></ToastContainer>
             <Navbar />
-            <div className='loginPage'>
-                <div className='loginPage-header'>
-                    <h1>Login to your Account</h1>
-                    <h3>Please login with your credentials</h3>
-                    <div className='loginPage-login'>
-                        <form>
-                            <input ref={emailRef} type='text' placeholder='Username' />
-                            <input ref={passwordRef} type={(
-                                showPassword ? 'text' : 'password')} placeholder='Password'
-                                style={{ marginBottom: '3px' }} />
-                            <div className="showPasswordDiv">
-                                <div className="showPasswordBox">
-                                    <input type='checkbox' onClick={() => {
-                                        if (showPassword) {
-                                            setShowPassword(false);
-                                            console.log(showPassword);
-                                        }
-                                        else {
-                                            setShowPassword(true);
-                                            console.log(showPassword);
-                                        }
-                                    } } />
-                                    <p>Show Password</p>
+            <EarthCanvas>
+                <div className='loginPage'>
+                    <div className='loginPage-header'>
+                        <h1>Login to your Account</h1>
+                        <h3>Please login with your credentials</h3>
+                        <div className='loginPage-login'>
+                            <form>
+                                <input ref={emailRef} type='text' placeholder='Username' />
+                                <input ref={passwordRef} type={(
+                                    showPassword ? 'text' : 'password')} placeholder='Password'
+                                    style={{ marginBottom: '3px' }} />
+                                <div className="showPasswordDiv">
+                                    <div className="showPasswordBox">
+                                        <input type='checkbox' onClick={() => {
+                                            if (showPassword) {
+                                                setShowPassword(false);
+                                                console.log(showPassword);
+                                            }
+                                            else {
+                                                setShowPassword(true);
+                                                console.log(showPassword);
+                                            }
+                                        } } />
+                                        <p>Show Password</p>
+                                    </div>
+                                    <Link className='forgotLink' to='/signUp'>Forgot Password?</Link>
                                 </div>
-                                <Link className='forgotLink' to='/signUp'>Forgot Password?</Link>
-                            </div>
-                            <button onClick={(e) => {
-                                e.preventDefault();
-                                loginWithPassword();
-                            } }>Log In</button>
-                            <div className="dontAccount">
-                                Don't have an account?
-                                <Link className='dontAccountLink' to='/signUp'>Sign Up</Link>
-                            </div>
-
-                        </form>
+                                <button onClick={(e) => {
+                                    e.preventDefault();
+                                    loginWithPassword();
+                                } }>Log In</button>
+                                <div className="dontAccount">
+                                    Don't have an account?
+                                    <Link className='dontAccountLink' to='/signUp'>Sign Up</Link>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-                <ToastContainer></ToastContainer>
-            </div>
+            </EarthCanvas>
         </>
     )
 }
